ba705382bff582c78f3127ede060dc9aa7817117
divine <divineobi07@gmail.com>
2022-05-21 14:12:29 +0100

    create a book card compnany for each image

---
+++ src/components/BookCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import firstImage from "../assets/backstagetalks_cover_issue_3.webp";
+import './BookCard.css'
+
+
+const BookCard = () => {
+  return (
+    <article className="container">
+      <img className="image" src={firstImage} alt="A blue book" />
+    </article>
+  );
+};
+
+
+export default BookCard;
