ec48b2053a04149f78983a6053e49e69685445f3
divine <divineobi07@gmail.com>
2022-05-21 14:50:03 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Logo from './assets/logo.webp'
+import BookList from "./components/BookList/BookList";
 import './App.css'
 
 const App = () => {
     return (
         <div className="wrapper">
             <header className="logo"><img src={Logo} alt="backstage logo"/></header>
+            <BookList />
         </div>
+
     )
 }
 
