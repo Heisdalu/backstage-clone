432692275c335e69d2f1471b4cf6a0061105d954
divine <divineobi07@gmail.com>
2022-05-21 13:39:19 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'
+// import lol from './assets/backstagetalks_cover_issue_3.webp'
+// C:\Users\HP\Desktop\DIVINE\backstage-clone\src\assets\backstagetalks_cover_issue_3.png
+
+const App = () => {
+    return (
+        <div className="wrapper">lol</div>
+    )
+}
+
+export default App;
