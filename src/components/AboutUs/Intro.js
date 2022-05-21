edf553269fd16bc1be7657f87f80b8ea3e111740
divine <divineobi07@gmail.com>
2022-05-21 22:18:30 +0100

    Intro component

---
+++ src/components/AboutUs/Intro.js
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./Intro.css";
+
+const Intro = () => {
+  return (
+    <>
+      <section className="intro_section">
+        <section className="intro_text">
+          <p>
+            Backstage Talks is a magazine of casual, but in depth dialogues on
+            design and business. Our decisions shape and influence this complex
+            world-to have a chance to make the right ones, we need to talk.
+          </p>
+          <section className="copyright">
+            &copy; 2022 <a href="#">Published by studio Milk</a>
+          </section>
+        </section>
+
+        <section className="privacy_policy">
+          <a href="#">Privacy Policy</a>
+        </section>
+        <section className="email_link">
+          <a href="#">info@backstagetalks.com</a>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default Intro;
