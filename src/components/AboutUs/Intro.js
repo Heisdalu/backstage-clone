7e73f63bd68e69ccb4f41fb4340efc8173b3f813
divine <divineobi07@gmail.com>
2022-05-21 23:57:07 +0100

    re-edit the intro component

--- src/components/AboutUs/Intro.js
+++ src/components/AboutUs/Intro.js
@@ -5,25 +5,23 @@ import "./Intro.css";
 const Intro = () => {
   return (
     <>
-      <footer className="intro_section">
-        <section className="intro_text">
-          <p>
-            Backstage Talks is a magazine of casual, but in depth dialogues on
-            design and business. Our decisions shape and influence this complex
-            world-to have a chance to make the right ones, we need to talk.
-          </p>
-          <section className="copyright">
-            &copy; 2022 <a href="#">Published by studio Milk</a>
-          </section>
+      <section className="intro_text">
+        <p>
+          Backstage Talks is a magazine of casual, but in depth dialogues on
+          design and business. Our decisions shape and influence this complex
+          world—to have a chance to make the right ones, we need to talk.
+        </p>
+        <section className="copyright">
+          &copy; 2022 <a href="#">Published by studio Milk</a>
         </section>
-
         <section className="privacy_policy">
           <a href="#">Privacy Policy</a>
         </section>
-        <section className="email_link">
-          <a href="#">info@backstagetalks.com</a>
-        </section>
-      </footer>
+      </section>
+
+      <section className="email_link">
+        <a href="#">info@backstagetalks.com</a>
+      </section>
     </>
   );
 };
