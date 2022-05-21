6a9ad2a4d16829a01bfb151e78ed6e92258be194
divine <divineobi07@gmail.com>
2022-05-21 14:48:58 +0100

    create new bookList folder,insert bookCard component, create bookList component

---
+++ src/components/BookList/BookList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BookCard from "./BookCard";
+
+
+const BookList = () => {
+    return <section className=".book__list">
+        <BookCard />
+    </section>;
+}
+
+export default BookList
